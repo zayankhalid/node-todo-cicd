606744549fdadbfbc69dc32a6b5b601fb79bc90a
Zayan Khalid <zayankhalid.pk@gmail.com>
2023-11-22 17:51:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.get('/todo', function (req, res) {
 
     .listen(port, function () {
         // Logging to console
-        console.log(`Todolist running on http://0.0.0.0:${port}`)
+        console.log(`Todolist running on the http://0.0.0.0:${port}`)
     });
 // Export app
 module.exports = app;
